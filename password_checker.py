a9bd7ca714809e883ffec9ec3b37a486efbd5745
Thomas Nutter <99580613+meadowshands@users.noreply.github.com>
2022-04-07 21:28:17 -0500

    Create password_checker.py

---
+++ password_checker.py
@@ -0,0 +1,11 @@
+import re
+
+verify_email = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")    # Verify email syntax
+verify_password = re.compile(r"[A-Za-z0-9!@#$%]{8,}[\d]$")                          # Verify password criteria
+email = 'testaccount7@gmail.com'
+password = 'TestpasW0rd!234'
+
+a = re.fullmatch(verify_email, email)
+b = re.fullmatch(verify_password, password)
+print(a)
+print(b)
